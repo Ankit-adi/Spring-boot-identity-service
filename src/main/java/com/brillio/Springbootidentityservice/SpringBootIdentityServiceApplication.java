131c8184eb72298b4b5c16bf9400aa08982b3e13
Ankit Aditya <Ankit.Aditya@BRILLIO.COM>
2023-05-11 12:55:59 +0530

    Initial commit

---
+++ src/main/java/com/brillio/Springbootidentityservice/SpringBootIdentityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.brillio.Springbootidentityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIdentityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIdentityServiceApplication.class, args);
+	}
+
+}
